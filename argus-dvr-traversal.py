7f636b720ae7365416782dbb2f4a26e95ce9c258
Alvin Aries Tapia <69188149+t4ps3c@users.noreply.github.com>
2024-07-28 16:08:43 +0800

    Update argus-dvr-traversal.py

--- argus-dvr-traversal.py
+++ argus-dvr-traversal.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Perform directory traversal.")
     parser.add_argument("ip", help="Target IP address")
     parser.add_argument("port", type=int, help="Target port")
-    parser.add_argument("target_file", help="File to access with directory traversal")
+    parser.add_argument("target_filepath", help="File path to access with directory traversal")
     
     args = parser.parse_args()
     
